5322288c1c127045ba448693ec6568ab201b6aa1
Lissantanad <153249528+Lissantanad@users.noreply.github.com>
2024-02-18 22:53:34 -0400

    Agregar archivos de proyecto.

---
+++ determina si es positivo o negativo/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese un número: ");
+        int numero = Convert.ToInt32(Console.ReadLine());
+
+        if (numero > 0)
+        {
+            Console.WriteLine("El número es positivo.");
+        }
+        else if (numero < 0)
+        {
+            Console.WriteLine("El número es negativo.");
+        }
+        else
+        {
+            Console.WriteLine("El número es cero.");
+        }
+    }
+}
